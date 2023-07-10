de7acab4ed2a6f30cc7c3f54421193c02307e14c
Ulyamaev Ruslan <rulyamaev.2003@mail.ru>
2023-07-10 20:24:10 +0500

    Поменял способ создания файлов

--- script.php
+++ script.php
@@ -1,16 +1,54 @@
 <?php
 session_start();
 
-$_SESSION['date'] = date('d.m.Y');                           //переменные
+#файл html шаблона
+$htmlFile = 'templateHtml.php';
+
+#файл шаблона текста
+$textFile ='text.php';
+
+#переменна для готовой html страницы
+$modifiedHtml =' ';
+
+$arrPath = [
+    1 => './agreeement',
+    2 => './consent',
+    3 => './privacy',
+    4 => './offer',
+    5 => './cookie'
+];
+
+#тут можно выбрать один из желаемых путей
+$generatedPath = $arrPath[1]; 
+
+
+#Данные вводимые пользователем
+$_SESSION['date'] = date('d.m.Y');                         
 $_SESSION['operator'] = 'ИП Улямаев Руслан Ильдарович';
 $_SESSION['domain'] = 'https://'.$_SERVER['HTTP_HOST'];
+$_SESSION['consent'] = $_SESSION['domain'] . '/consent';
+$_SESSION['privacy'] = $_SESSION['domain'] . '/privacy';
+$_SESSION['agreement'] = $_SESSION['domain'] . '/agreement';
 
-echo $_SESSION['date'];
 
-#require 'template.php';
-#require 'template.phtml';
+#получение текста
+$text = file_get_contents($textFile);
 
-$templateHtml = file_get_contents('template.phtml');        //помещаю html страницу в переменную $templateHtml
-#echo $templateHtml;
+#получение шаблона html
+$templateHtml = file_get_contents($htmlFile);        
 
-file_put_contents('index.php', $templateHtml );             //генерация готового файла
+
+#поиск позиции для вставки текста
+$insert_position = strpos($templateHtml, "</body>"); 
+
+
+//проверка есть ли место куда вставить текст
+if ($insert_position !== false) {
+    $modifiedHtml = substr_replace($templateHtml, $text, $insert_position);
+    @mkdir($generatedPath);
+    file_put_contents($generatedPath . '/index.php', $modifiedHtml );  //генерация готового файла 
+
+    echo $modifiedHtml;
+} else {
+    echo "Место для вставки текста не найдено.";
+}
--- templateHtml.php
+++ templateHtml.php
@@ -3,7 +3,6 @@
        <title>Готовый документ</title>
    </head>
    <body>
-       <h1>Готовый документ</h1>
-       <?php require 'template.php'; ?>
+    
    </body>
    </html>
